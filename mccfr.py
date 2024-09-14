f61fc781f4644eb0120c84610fe26cf1c12b6f0d
Azerus96 <gencinski1996@gmail.com>
2024-09-14 05:29:12 +0300

    Update mccfr.py

--- mccfr.py
+++ mccfr.py
@@ -1,5 +1,3 @@
-# mccfr.py
-
 import random
 from collections import defaultdict
 
@@ -9,28 +7,33 @@ class MCCFR:
         self.iterations = iterations
         self.strategy = defaultdict(lambda: [0, 0])  # Хранение стратегии: [регреты, кумулятивная стратегия]
 
-    def run_iterations(self, game_history):
-        """Запускает итерации MCCFR для улучшения стратегии."""
-        for _ in range(self.iterations):
+    def run_iterations(self, game_history, iterations=None):
+        """Запуск итераций MCCFR для улучшения стратегии."""
+        if iterations is None:
+            iterations = self.iterations
+        
+        for _ in range(iterations):
             self.run_simulation(game_history)
 
     def run_simulation(self, game_history):
         """Запуск симуляции по дереву решений."""
-        for action in ['fold', 'call', 'raise']:
+        actions = ['fold', 'call', 'raise']
+        for action in actions:
             self.update_regret(game_history, action)
 
     def update_regret(self, game_history, action):
+        """Обновление регрета для конкретного действия."""
         action_probability = self.calculate_action_probability(action)
         regret = self.calculate_regret(game_history, action)
         self.strategy[action][0] += regret  # Обновление регретов
-        self.strategy[action][1] += action_probability  # Обновление стратегии
+        self.strategy[action][1] += action_probability  # Обновление кумулятивной стратегии
 
     def calculate_action_probability(self, action):
-        """Рассчитывает вероятность выполнения данного действия на основе текущей стратегии."""
+        """Рассчитывает вероятность выполнения действия на основе текущей стратегии."""
         cumulative = sum(max(self.strategy[a][0], 0) for a in self.strategy)
         if cumulative > 0:
             return max(self.strategy[action][0], 0) / cumulative
-        return 1.0 / len(self.strategy)
+        return 1.0 / len(self.strategy)  # Равная вероятность для всех действий если нет регрета
 
     def calculate_regret(self, game_history, action):
         """Рассчитывает сожаление за выполнение данного действия в игровом контексте."""
@@ -39,15 +42,15 @@ class MCCFR:
         return payoffs[action] - expected_payoff
 
     def get_payoffs_for_actions(self, game_history):
-        """Рассчитывает ожидаемую выгоду от различных действий на базе истории хода игры."""
-        # Примерный расчет на основе ставки
+        """Рассчитывает ожидаемую выгоду от различных действий на основе истории хода игры."""
+        current_bet = game_history[-1]["current_bet"]
         return {
-            'fold': -game_history[-1]["current_bet"],
-            'call': random.randint(-game_history[-1]["current_bet"], game_history[-1]["current_bet"]),
-            'raise': random.randint(1, 2 * game_history[-1]["current_bet"]),
+            'fold': -current_bet,
+            'call': random.randint(-current_bet, current_bet),
+            'raise': random.randint(current_bet, 2 * current_bet),
         }
 
     def decide(self, game_state, dossier):
-        """Принятие решения на основе оптимизированной стратегии."""
+        """Принятие решения на основе оптимизированной стратегии MCCFR."""
         self.run_iterations(game_state)
-        return max(self.strategy, key=lambda x: self.strategy[x][1])  # Действие с наибольшей общей стратегией
+        return max(self.strategy, key=lambda x: self.strategy[x][1])  # Действие с наибольшей кумулятивной стратегией
